801419a99ea754fdc5c0812cfdfd8bf9373c59a8
Nick Tong <32203145+nt314p@users.noreply.github.com>
2017-10-26 20:48:16 -0400

    changes to CustomTarget

--- MyRobots/src/robots/CustomTarget.java
+++ MyRobots/src/robots/CustomTarget.java
@@ -6,13 +6,12 @@ public class CustomTarget extends Robot {
 
 	public void run() {
 
+		turnLeft(getHeading());
+
 		while (true) {
-			if (!((getHeading() < 91) && (getHeading() > 89))) {
-				turnRight((getHeading()-90)/2 );
-			} else {
-				ahead(100);
-				ahead(-100);
-			}
+
+			ahead(500);
+			ahead(-500);
 		}
 	}
 }
