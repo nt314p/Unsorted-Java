c23b63e3eaa96ba14c1e6c6b2709c93a8b02d26c
Nick Tong <32203145+nt314p@users.noreply.github.com>
2017-11-14 17:56:03 -0500

    prime spiral gen testing

---
+++ Prime_Spiral/src/PrimeSpiralGenerator.java
@@ -0,0 +1,33 @@
+import processing.core.PApplet;
+
+public class PrimeSpiralGenerator extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("PrimeSpiralGenerator");
+	}
+	
+	int sWidth = 400;
+	int sHeight = 400;
+	int scale = 10;
+	
+	int currX = Math.round(sWidth/2);
+	int currY = Math.round(sHeight/2);
+	
+	int xDir = 1;
+	int yDir = 1;
+	int len = 1;
+
+	public void settings() {
+		size(sWidth, sHeight);
+	}
+
+	public void setup() {
+		frameRate(60);
+	}
+
+	public void draw() {
+		
+	}
+
+}
