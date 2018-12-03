6ff48c9ac0bea38e86557c3e46732cb864529745
Nicolas Tong <ntong@bayviewglen.ca>
2018-12-03 12:35:08 -0500

    Coding bat testing

--- Tester/src/testing/CodebatKYS.java
+++ Tester/src/testing/CodebatKYS.java
@@ -4,10 +4,17 @@ public class CodebatKYS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String str = " hello there hi me";
-		
 
+		System.out.println(withoutDoubles(3, 3, true));
+
+	}
+
+	public static int withoutDoubles(int die1, int die2, boolean noDoubles) {
+		if (noDoubles && die1 == die2) {
+			die2 = ((die2 - 1) % 5) + 1;
+			System.out.println(die2);
+		}
+		return die1+die2;
 	}
 
 }
--- Tester/src/testing/TEST.java
+++ Tester/src/testing/TEST.java
@@ -6,7 +6,25 @@ public class TEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Double.MAX_VALUE);
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			System.out.print("Hand: ");
+			System.out.println(isFlush(in.nextLine()));
+		}
+	}
+	
+	public static boolean isFlush(String hand) {
+		char suit = '0';
+		String[] cards = hand.split(" ");
+		for (int i = 0; i < cards.length; i++) {
+			char currSuit = cards[i].charAt(cards[i].length()-1);
+			if (suit=='0') {
+				suit = currSuit;
+			} else if (!(currSuit == suit)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
